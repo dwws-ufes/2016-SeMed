2830654a560045d38f1c990b5e3f139e660704b3
claudenirmf <claudenirmf@gmail.com>
2016-04-26 15:54:41 -0300

    Commit

--- SeMed/src/br/ufes/inf/nemo/semed/control/ManagePatientController.java
+++ SeMed/src/br/ufes/inf/nemo/semed/control/ManagePatientController.java
@@ -1,10 +1,13 @@
 package br.ufes.inf.nemo.semed.control;
 
+import java.util.Date;
+
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import br.ufes.inf.nemo.semed.application.ManagePatientService;
+import br.ufes.inf.nemo.semed.exception.InvalidCpfException;
 import br.ufes.inf.nemo.semed.model.Patient;
 import br.ufes.inf.nemo.util.ejb3.application.CrudService;
 import br.ufes.inf.nemo.util.ejb3.controller.CrudController;
@@ -16,6 +19,7 @@ public class ManagePatientController extends CrudController<Patient> {
 	
 	public ManagePatientController() {
 		viewPath = "/managePatients/";
+		bundleName = "msgs";
 	}
 	
 	@Override
