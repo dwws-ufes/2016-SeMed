1e845f70a7f6f80f16f5af872afbcb260b6a9989
claudenirmf <claudenirmf@gmail.com>
2016-06-28 21:09:17 -0300

    Correções no HTML e na ontologia

--- SeMed/src/br/ufes/inf/nemo/semed/servlets/NSUtils.java
+++ SeMed/src/br/ufes/inf/nemo/semed/servlets/NSUtils.java
@@ -5,6 +5,7 @@ public class NSUtils {
 	public static final String SEMED_NS = "http://www.semed.com/semed_ontology#";
 	public static final String SEMED_PREFIX = "semed";
 	
+	public static final String SEMED_ADDRESS = SEMED_NS + "Address";
 	public static final String SEMED_DISEASE = SEMED_NS + "Disease";
 	public static final String SEMED_DOCTOR = SEMED_NS + "Doctor";
 	public static final String SEMED_DRUG = SEMED_NS + "Drug";
@@ -12,9 +13,10 @@ public class NSUtils {
 	public static final String SEMED_PATIENT = SEMED_NS + "Patient";
 	public static final String SEMED_PERSON = SEMED_NS + "Person";
 	
-	public static final String SEMED_PROP_DISEASE = SEMED_NS + "disease";
+	public static final String SEMED_PROP_ADDRESS = SEMED_NS + "address";
+	public static final String SEMED_PROP_DIAGNOSED_DISEASE = SEMED_NS + "diagnosedDisease";
 	public static final String SEMED_PROP_DOCTOR = SEMED_NS + "doctor";
-	public static final String SEMED_PROP_DRUG = SEMED_NS + "drug";
+	public static final String SEMED_PROP_DRUGS_PRESCRIPTION = SEMED_NS + "drugsPrecription";
 	public static final String SEMED_PROP_PATIENT = SEMED_NS + "patient";
 	
 	public static final String SEMED_PROP_BIRTH_DATE = SEMED_NS + "birthDate";
@@ -22,14 +24,12 @@ public class NSUtils {
 	public static final String SEMED_PROP_CPF = SEMED_NS + "cpf";
 	public static final String SEMED_PROP_DATE = SEMED_NS + "date";
 	public static final String SEMED_PROP_FIRST_NAME = SEMED_NS + "firstName";
-	public static final String SEMED_PROP_DISEASE_NAME = SEMED_NS + "name1";
-	public static final String SEMED_PROP_DRUG_NAME = SEMED_NS + "name2";
+	public static final String SEMED_PROP_DISEASE_NAME = SEMED_NS + "diseaseName";
+	public static final String SEMED_PROP_DRUG_NAME = SEMED_NS + "drugName";
 	public static final String SEMED_PROP_NUMBER = SEMED_NS + "number";
 	public static final String SEMED_PROP_OBSERVATIONS = SEMED_NS + "observations";
 	public static final String SEMED_PROP_PROFESSIONAL_ID = SEMED_NS + "professionalId";
 	public static final String SEMED_PROP_RECOMMENDATIONS = SEMED_NS + "recommendations";
-	public static final String SEMED_PROP_DRUG_SOURCE = SEMED_NS + "referenceResource1";
-	public static final String SEMED_PROP_DISEASE_SOURCE = SEMED_NS + "referenceResource2";
 	public static final String SEMED_PROP_STATE = SEMED_NS + "state";
 	public static final String SEMED_PROP_STREET = SEMED_NS + "street";
 	public static final String SEMED_PROP_SURNAME = SEMED_NS + "surname";
@@ -40,5 +40,13 @@ public class NSUtils {
 	public static final String GR_FULL_NAME = GR_NS + "FullName";
 	public static final String GR_CPF = GR_NS + "Cpf";
 	public static final String GR_BIRTH_DATE = GR_NS + "BirthDate";
+	
+	public static final String DRUGBANK_NS = "http://bio2rdf.org/drugbank_vocabulary:";
+	public static final String DRUGBANK_PREFIX = "drugbank_vocabulary";
+	public static final String DRUGBANK_DRUG = "Drug";
+	
+	public static final String BIO2RDF_NS = "http://bio2rdf.org/pharmgkb_vocabulary:";
+	public static final String BIO2RDF_PREFIX = "pharmgkb_vocabulary";
+	public static final String BIO2RDF_DISEASE = "Disease";
 
 }
